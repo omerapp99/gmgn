4d05593c8195141fdb4501bb8c45e5e391afd1dc
Omer Itach <itachomer@gmail.com>
2025-02-27 10:36:15 +0200

    Init commit

---
+++ background.js
@@ -0,0 +1,23 @@
+  browser.runtime.onInstalled.addListener(() => {
+    browser.contextMenus.create({
+      id: "search-gmgn",
+      title: "Search on GMGN.ai",
+      contexts: ["link"]
+    });
+  });
+  
+  browser.contextMenus.onClicked.addListener((info, tab) => {
+    if (info.menuItemId === "search-gmgn") {
+      const link = info.linkUrl;
+      const solscanPattern = /https:\/\/solscan\.io\/account\/([^\/]+)/;
+      const match = link.match(solscanPattern);
+      
+      if (match) {
+        const walletAddress = match[1];
+        
+        browser.tabs.create({
+          url: `https://gmgn.ai/sol/address/${walletAddress}`
+        });
+      }
+    }
+  });
